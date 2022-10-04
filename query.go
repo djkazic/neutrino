004be8beba880b8fff280fb94e493cf5a8235767
Ruben Børseth <ruben.beck@pm.me>
2022-10-04 12:36:58 +0200

    cleanup whitespace..

--- query.go
+++ query.go
@@ -965,7 +965,6 @@ func (s *ChainService) GetCFilter(blockHash chainhash.Hash,
 				}
 				// imediatly calling on to handle the results
 				s.handleCFiltersResponse(query, filter, quit)
-
 			}
 		} else {
 			s.queryPeers(
