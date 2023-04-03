a2aab1ed3dd6ca70b8a09724858fa7f3b0ae3c7c
ruben beck <ruben.beck@pm.me>
2023-04-03 13:00:45 +0200

    use 10 sec since last failure, ignore gosec warning

--- query_rest.go
+++ query_rest.go
@@ -3,7 +3,6 @@ package neutrino
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"time"
@@ -16,7 +15,7 @@ func (s *ChainService) queryRestPeers(query *cfiltersQuery) {
 	client := &http.Client{}
 	validPeers := make([]int, 0, len(s.restPeers))
 	for i, p := range s.restPeers {
-		if p.failures == 0 || time.Since(p.lastFailure) > QueryBatchTimeout {
+		if p.failures == 0 || time.Since(p.lastFailure) > 10*time.Second {
 			validPeers = append(validPeers, i)
 		}
 	}
@@ -24,6 +23,7 @@ func (s *ChainService) queryRestPeers(query *cfiltersQuery) {
 		log.Errorf("queryRestPeers - No valid rest peer")
 		return
 	}
+	// #nosec G404 -- No need to have true randomness when selecting restpeer.
 	restPeerIndex := validPeers[rand.Intn(len(validPeers))]
 	URL := fmt.Sprintf("%v/rest/blockfilter/basic/%v.bin?count=%v", s.restPeers[restPeerIndex].URL, query.stopHash.String(), query.stopHeight-query.startHeight+1)
 	log.Infof("getting %v filters from height %v to height %v, using URL: %v", query.stopHeight-query.startHeight+1, query.startHeight, query.stopHeight, URL)
@@ -39,7 +39,7 @@ func (s *ChainService) queryRestPeers(query *cfiltersQuery) {
 		s.restPeers[restPeerIndex].failures++
 		s.restPeers[restPeerIndex].lastFailure = time.Now()
 		log.Errorf("queryRestPeers - Get (%v) status != OK: %v", URL, res.Status)
-		_, err = io.Copy(ioutil.Discard, res.Body)
+		_, err = io.Copy(io.Discard, res.Body)
 		if err != nil {
 			log.Errorf("error in io.Copy: %v", err)
 		}
